4523e008d483efdb57239da23694de19c72ddd24
Aaron Blais <abmalachi@gmail.com>
2023-07-13 11:14:26 -0400

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/AaronBlais12/pokedex.git",
+  base: "/pokedex.git/",
   plugins: [react()],
 })
