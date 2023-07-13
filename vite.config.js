bf972daf230322ab5285daeba4b09df4a947822f
Aaron Blais <abmalachi@gmail.com>
2023-07-13 10:48:19 -0400

    added deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://github.com/AaronBlais12/pokedex.git",
   plugins: [react()],
 })
